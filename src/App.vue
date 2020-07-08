b7f49f5502db47681257cd4121adb9b4d716c868
Rafael Paraná <paranafael@yahoo.com.br>
2020-07-08 17:07:55 -0300

    Components TaskGrid e NewTask

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,65 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<new-task @taskAdded="addTask"/>
+		<TaskGrid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChanged="toggleTaskState"/>
 	</div>
 </template>
 
 <script>
-export default {
+import NewTask from "./components/NewTask.vue";
+import TaskGrid from "./components/TaskGrid.vue";
 
-}
+export default {
+  components: {
+    NewTask,
+    TaskGrid,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(task) {
+      const sameName = (t) => t.name === task.name;
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true,
+        });
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    toggleTaskState(index) {
+      this.tasks[index].pending = !this.tasks[index].pending;
+    },
+  },
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
